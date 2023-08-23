3aa1e71a7e6a21bdf6ae54df3e3a194e417dec80
TpL <tranphilong030201@gmail.com>
2023-08-23 09:55:38 +0700

    test-3

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Layout from './Layout/Layout';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 const HomePage = React.lazy(() => import('./Page/HomePage/HomePage'));
 const Portfolio = React.lazy(() => import('./Page/Portfolio/Portfolio'));
