7bd47f8d2e2fd0808854ce115c3df817165b5af2
TpL <tranphilong030201@gmail.com>
2023-08-25 10:28:30 +0700

    update-25-8_2

--- src/Page/Portfolio/tabs/Tab12.jsx
+++ src/Page/Portfolio/tabs/Tab12.jsx
@@ -4,15 +4,15 @@ import img_1 from '../../../assets/imgs/tab12/tab12.1.JPG';
 import img_1x600 from '../../../assets/imgs/tab12/tab12.1x600.jpg';
 import img_2 from '../../../assets/imgs/tab12/tab12.2.JPG';
 import img_2x600 from '../../../assets/imgs/tab12/tab12.2x600.jpg';
-import img_4 from '../../../assets/imgs/tab12/tab12.4.jpg';
+import img_4 from '../../../assets/imgs/tab12/tab12.4.JPG';
 import img_4x600 from '../../../assets/imgs/tab12/tab12.4x600.jpg';
-import img_5 from '../../../assets/imgs/tab12/tab12.5.jpg';
+import img_5 from '../../../assets/imgs/tab12/tab12.5.JPG';
 import img_5x400 from '../../../assets/imgs/tab12/tab12.5x420.jpg';
-import img_6 from '../../../assets/imgs/tab12/tab12.6.jpg';
+import img_6 from '../../../assets/imgs/tab12/tab12.6.JPG';
 import img_6x400 from '../../../assets/imgs/tab12/tab12.6x420.jpg';
-import img_7 from '../../../assets/imgs/tab12/tab12.7.jpg';
+import img_7 from '../../../assets/imgs/tab12/tab12.7.JPG';
 import img_7x400 from '../../../assets/imgs/tab12/tab12.7x420.jpg';
-import img_8 from '../../../assets/imgs/tab12/tab12.8.jpg';
+import img_8 from '../../../assets/imgs/tab12/tab12.8.JPG';
 import img_8x400 from '../../../assets/imgs/tab12/tab12.8x420.jpg';
 
 const Tab12 = () => {
