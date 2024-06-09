bbfd87e15340d73b410aff399448d42e5a8ebf0e
TpL <tranphilong030201@gmail.com>
2024-06-09 20:51:19 +0700

    update 9/6

--- src/Page/Portfolio/tabs/Tab10.jsx
+++ src/Page/Portfolio/tabs/Tab10.jsx
@@ -10,10 +10,14 @@ import img_4 from '../../../assets/imgs/tab10/tab10.4.JPG';
 import img_4cp from '../../../assets/imgs/tab10/tab10.4cp.JPG';
 import img_5 from '../../../assets/imgs/tab10/tab10.5.JPG';
 import img_5cp from '../../../assets/imgs/tab10/tab10.5cp.JPG';
-import img_6 from '../../../assets/imgs/tab10/tab10.6.PNG';
-import img_6cp from '../../../assets/imgs/tab10/tab10.6cp.PNG';
-import img_7 from '../../../assets/imgs/tab10/tab10.7.PNG';
-import img_7cp from '../../../assets/imgs/tab10/tab10.7cp.PNG';
+import img_6 from '../../../assets/imgs/tab10/tab10.6.JPG';
+import img_6cp from '../../../assets/imgs/tab10/tab10.6cp.JPG';
+import img_7 from '../../../assets/imgs/tab10/tab10.7.JPG';
+import img_7cp from '../../../assets/imgs/tab10/tab10.7cp.JPG';
+import img_8 from '../../../assets/imgs/tab10/tab10.8.JPG';
+import img_8cp from '../../../assets/imgs/tab10/tab10.8cp.JPG';
+import img_9 from '../../../assets/imgs/tab10/tab10.9.JPG';
+import img_9cp from '../../../assets/imgs/tab10/tab10.9cp.JPG';
 
 const Tab10 = () => {
     return (
@@ -29,14 +33,14 @@ const Tab10 = () => {
                 </div>
             </a>
 
-            <a href={img_2} data-fancybox="tab10">
+            <a href={img_5} data-fancybox="tab10">
                 <div
                     className="relative img_block"
                     onClick={() => {
                         Fancybox.bind('[data-fancybox="tab10"]', {});
                     }}
                 >
-                    <img className="image_popup" src={img_2cp} alt="" />
+                    <img className="image_popup" src={img_5cp} alt="" />
                 </div>
             </a>
 
@@ -62,14 +66,14 @@ const Tab10 = () => {
                 </div>
             </a>
 
-            <a href={img_5} data-fancybox="tab10">
+            <a href={img_2} data-fancybox="tab10">
                 <div
                     className="relative img_block"
                     onClick={() => {
                         Fancybox.bind('[data-fancybox="tab10"]', {});
                     }}
                 >
-                    <img className="image_popup" src={img_5cp} alt="" />
+                    <img className="image_popup" src={img_2cp} alt="" />
                 </div>
             </a>
 
@@ -92,6 +96,28 @@ const Tab10 = () => {
                     }}
                 >
                     <img className="image_popup" src={img_7cp} alt="" />
+                </div>
+            </a>
+
+            <a href={img_8} data-fancybox="tab10">
+                <div
+                    className="relative img_block"
+                    onClick={() => {
+                        Fancybox.bind('[data-fancybox="tab10"]', {});
+                    }}
+                >
+                    <img className="image_popup" src={img_8cp} alt="" />
+                </div>
+            </a>
+
+            <a href={img_9} data-fancybox="tab10">
+                <div
+                    className="relative img_block"
+                    onClick={() => {
+                        Fancybox.bind('[data-fancybox="tab10"]', {});
+                    }}
+                >
+                    <img className="image_popup" src={img_9cp} alt="" />
                 </div>
             </a>
         </div>
